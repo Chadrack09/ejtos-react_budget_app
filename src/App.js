c2bb6f71e8deb8c266c4dfb5c05ca8039bb798e2
Chaadrack <bbenolych@gmail.com>
2023-01-23 09:56:43 -0500

    made changes to file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Add code to import the components
 import { AppProvider } from './context/AppContext';
+import Budget from './components/Budget';
+import AllocationForm from './components/AllocationForm';
+import ExpenseItem from './components/ExpenseItem';
+import ExpenseList from './components/ExpenseList';
+import ExpenseTotal from './components/ExpenseTotal';
+import Remaining from './components/Remaining';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                {/* Add the code here to add the components          */}
+                <AllocationForm />
+                <Budget />
+                <ExpenseItem />
+                <ExpenseList />
+                <ExpenseTotal />
+                <Remaining />
             </div>
         </AppProvider>
     );
